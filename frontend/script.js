dd38f054b3de6452177cdad928a13c558eb0caa9
Josh Richards <joshuarichards001@gmail.com>
2024-08-26 23:59:48 +0000

    Revert updated approach to color buttons

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,14 @@
 const colorMap = {
-  0: "white",
-  1: "green",
-  2: "yellow",
-  3: "red",
-  4: "orange",
-  5: "purple",
-  6: "blue",
-  7: "teal",
-  8: "pink",
-  9: "black",
+  0: "#FFFFFF", // white
+  1: "#74B63E", // green
+  2: "#FFCE33", // yellow
+  3: "#CC421D", // red
+  4: "#FF8533", // orange
+  5: "#87308C", // purple
+  6: "#1D70A2", // blue
+  7: "#079D9D", // teal
+  8: "#F05689", // pink
+  9: "#000000", // black
 };
 
 window.onload = () => {
