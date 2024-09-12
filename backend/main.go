964966226d75ad11aaabc2ccfa919e1b13e9a454
DeedleFake <deedlefake@users.noreply.github.com>
2024-09-12 15:30:53 -0400

    backend: shut down the server cleanly when the context is canceled

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 )
+
+func runHTTPServer(ctx context.Context) {
+	server := &http.Server{
+		Addr: ":8080",
+	}
+
+	shutdown := make(chan error, 1)
+	go func() {
+		<-ctx.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdown <- server.Shutdown(ctx)
+	}()
+
+	fmt.Println("Server is running on :8080")
+	err := server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	err = <-shutdown
+	if err != nil {
+		log.Fatal("Shutdown: ", err)
+	}
+}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -19,10 +48,5 @@ func main() {
 
 	http.HandleFunc("/ws", server.handleConnections)
 	http.HandleFunc("/pixels", corsMiddleware(server.handleGetPixels))
-
-	fmt.Println("Server is running on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	runHTTPServer(ctx)
 }
